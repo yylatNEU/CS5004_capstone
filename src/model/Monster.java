7791c38645cff617dea128e09b644af127234864
jackson2000118-crypto <jackson2000118@gmail.com>
2026-03-29 19:22:42 -0400

    Monster and Puzzle

--- src/model/Monster.java
+++ src/model/Monster.java
@@ -1,4 +1,83 @@
 package model;
 
 public class Monster {
+    private final String name;
+    private boolean active;
+    private final boolean affectsTarget;
+    private final boolean affectsPlayer;
+    private final String solution;
+    private final int value;
+    private final String description;
+    private final String effects;
+    private final int damage;
+    private final String target;
+    private final boolean canAttack;
+    private final String attack;
+    
+    public Monster(String name, boolean active, boolean affectsTarget,
+                   boolean affectsPlayer, String solution, int value,
+                   String description, String effects, int damage,
+                   String target, boolean canAttack, String attack) {
+        this.name = name;
+        this.active = active;
+        this.affectsTarget = affectsTarget;
+        this.affectsPlayer = affectsPlayer;
+        this.solution = solution;
+        this.value = value;
+        this.description = description;
+        this.effects = effects;
+        this.damage = damage;
+        this.target = target;
+        this.canAttack = canAttack;
+        this.attack = attack;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+
+    public boolean isActive()      { 
+        return active; 
+    }
+
+    public boolean affectsTarget() { 
+        return affectsTarget; 
+    }
+
+    public boolean affectsPlayer() { 
+        return affectsPlayer; 
+    }
+    public String getSolution() { 
+        return solution; 
+    }
+    public int getValue() { 
+        return value; 
+    }
+    public String getDescription() { 
+        return description; 
+    }
+    public String getEffects() { 
+        return effects; 
+    }
+    public int getDamage() { 
+        return damage; 
+    }
+    public String getTarget() {
+        return target; 
+    }
+    public boolean canAttack() { 
+        return canAttack; 
+    }
+    public String getAttack() { 
+        return attack; 
+    }
+
+    public void deactivate() { 
+        this.active = false; 
+    }
+
+    @Override
+    public String toString() { 
+        return name.toUpperCase(); 
+    }
 }
--- src/model/Puzzle.java
+++ src/model/Puzzle.java
@@ -1,4 +1,101 @@
 package model;
 
 public class Puzzle {
+    private final String name;
+    private boolean active;
+    private final boolean affectsTarget;
+    private final boolean affectsPlayer;
+    private final String solution;
+    private final int value;
+    private final String description;
+    private final String effects;
+    private final String target;
+
+    public Puzzle(String name, boolean active, boolean affectsTarget,
+              boolean affectsPlayer, String solution, int value,
+              String description, String effects, String target) {
+        this.name = name;
+        this.active = active;
+        this.affectsTarget = affectsTarget;
+        this.affectsPlayer = affectsPlayer;
+        this.solution = solution;
+        this.value = value;
+        this.description = description;
+        this.effects = effects;
+        this.target = target;
+    }
+
+    public String getName() { 
+        return name; 
+    }
+
+    public boolean isActive() { 
+        return active; 
+    }
+
+    public boolean affectsTarget() { 
+        return affectsTarget; 
+    }
+
+    public boolean affectsPlayer() { 
+        return affectsPlayer; 
+    }
+
+    public String getSolution() { 
+        return solution; 
+    }
+
+    public int getValue() { 
+        return value; 
+    }
+
+    public String getDescription() { 
+        return description; 
+    }
+
+    public String getEffects() { 
+        return effects; 
+    }
+
+    public String getTarget() { 
+        return target; 
+    }
+
+    public boolean isAnswerSolution() {
+        return solution != null
+            && solution.startsWith("'")
+            && solution.endsWith("'");
+    }
+
+    public String getCleanSolution() {
+    if (isAnswerSolution()) {
+        return solution.substring(1, solution.length() - 1);
+    }
+    return solution;
+    }
+
+    public void deactivate() {
+        this.active = false;
+    }
+
+    public boolean solve(String input) {
+        if (solution.startsWith("'")) {
+            return input.equals(getCleanSolution());
+        } 
+        else {
+            return input.equals(solution);
+        }
+    }
+
+    public boolean solveWithItem(String itemName) {
+        if (itemName == null) return false;
+        if (isAnswerSolution()) return false; 
+        return itemName.equalsIgnoreCase(solution);
+    }
+
+    @Override
+    public String toString() {
+        return name.toUpperCase();
+    }
 }
+
