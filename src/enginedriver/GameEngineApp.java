e74e6739039650785cf1be2d567f25a6cbde922a
jackson2000118-crypto <jackson2000118@gmail.com>
2026-03-31 22:27:57 -0400

    GameEngineApp

--- src/enginedriver/GameEngineApp.java
+++ src/enginedriver/GameEngineApp.java
@@ -1,4 +1,35 @@
 package enginedriver;
 
+import controller.GameController;
+import model.GameModel;
+import model.GameWorld;
+import model.IGameModel;
+import model.JsonGameLoader;
+
+import java.io.IOException;
+
 public class GameEngineApp {
+
+    private final String gameFileName;
+    private final Readable source;
+    private final Appendable output;
+
+    public GameEngineApp(String gameFileName, Readable source, Appendable output) {
+        this.gameFileName = gameFileName;
+        this.source = source;
+        this.output = output;
+    }
+
+    public void start() throws IOException {
+
+        JsonGameLoader loader = new JsonGameLoader();
+        GameWorld world = loader.load(gameFileName);
+
+        IGameModel model = new GameModel(world);
+
+        GameController controller = new GameController(model, source, output);
+
+        
+        controller.play();
+    }
 }
